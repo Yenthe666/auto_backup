d6dabd95acbd111eaf9f3098e9b7351c5b93429d
Magneticlab <info@magneticlab.ch>
2018-06-29 15:17:16 +0200

    Update db_backup.py
    Why was the sftp port hard coded to 22?

--- auto_backup/models/db_backup.py
+++ auto_backup/models/db_backup.py
@@ -119,7 +119,7 @@ class db_backup(models.Model):
             try:
                 s = paramiko.SSHClient()
                 s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                s.connect(ipHost, 22, usernameLogin, passwordLogin, timeout=10)
+                s.connect(ipHost, portHost, usernameLogin, passwordLogin, timeout=10)
                 sftp = s.open_sftp()
                 messageTitle = _("Connection Test Succeeded!\nEverything seems properly set up for FTP back-ups!")
             except Exception as e:
